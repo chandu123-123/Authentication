bdd8d8756c91ecda7a8573e8b23b454edd2f2486
chandu123-123 <103361757+chandu123-123@users.noreply.github.com>
2023-12-02 13:10:10 +0530

    pages are added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
-import React from "react"
-
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
 export default function App() {
   return (
-    <>
-    <h1 >
-    Hello world!
-  </h1>
+    <BrowserRouter>
+    <h1>hello</h1>
+    <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/about" element={<About/>}></Route>
 
-    </>
-    
+            <Route path="profile" element={<Profile/>}></Route>
+            <Route path="/sign-up" element={<Signup/>}></Route>
+            <Route path="/sign-in" element={<Signin/>}></Route>
+    </Routes>
+    </BrowserRouter>  
   )
 }
