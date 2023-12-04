6a02c4aa27ebf9d57ac28e65d5ab8aa1e0b8fb9f
chandu123-123 <103361757+chandu123-123@users.noreply.github.com>
2023-12-04 21:00:01 +0530

    mongodb connected

--- back/index.js
+++ back/index.js
@@ -1,8 +1,22 @@
-const express=require("express")
-const cors=require("cors")
-const app=express()
-app.use(express.json())
-app.use(cors())
-app.listen("3000",(req,res)=>{
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+
+    
+    console.log("database connected");
+}).catch((err) => {
+    console.log(err);
+});
+
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(3000, () => {
     console.log("server is running");
-})
+    
+});
